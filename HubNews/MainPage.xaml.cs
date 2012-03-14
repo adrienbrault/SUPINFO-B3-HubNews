7235af57025e485451b05f06e7678b1c903e0e60
Adrien Brault <monsti@gmail.com>
2012-03-14 16:44:12 +0100

    Initial commit

---
+++ HubNews/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace HubNews
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
